777511d1d824b701e65dbfaabc27e1b80a677096
Connor McDonald <connor.s.mcdonald@oracle.com>
2019-11-05 10:32:32 +0800

    Office Hours - real time statistics demo

---
+++ office-hours/18c_rts.sql
@@ -0,0 +1,82 @@
+set termout off
+conn USERNAME/PASSWORD@db18_pdb1
+set termout off
+@drop t
+@clean
+set termout off
+set echo on
+column table_name format a10 
+column num_rows format 9999999
+column column_name format a20
+column low_value format a15
+column high_value format a15
+column plan_table_output format a180
+col notes format a32
+set linesize 250
+set tab off
+set termout on
+select banner from v$version ;
+
+create table t (
+  id number(10), 
+  num number(10),
+  constraint t_pk primary key ( id )
+);
+pause
+
+insert /*+ APPEND */ into t select rownum,rownum from dual connect by rownum<=10000;
+exec dbms_stats.gather_table_stats(user,'t')
+pause
+
+clear screen
+
+select stale_stats from user_tab_statistics
+where  table_name = 'T';
+pause
+
+select table_name, num_rows, blocks
+from   user_tab_statistics 
+where  table_name = 'T';
+pause
+
+select table_name, column_name, low_value, high_value, num_distinct
+from   user_tab_col_statistics 
+where  table_name = 'T';
+pause
+
+clear screen
+
+select max(num),count(*) 
+from t where id > 9000;
+pause
+select *
+from table(dbms_xplan.display_cursor(format=>'typical'));
+pause
+
+clear screen
+insert into t select rownum+10000,rownum+10000 from dual connect by rownum<=1000;
+commit;
+pause
+exec dbms_stats.flush_database_monitoring_info;
+
+select stale_stats from user_tab_statistics
+where  table_name = 'T';
+pause
+
+select table_name, num_rows, blocks
+from   user_tab_statistics 
+where  table_name = 'T';
+pause
+
+select table_name, column_name, low_value, high_value
+from   user_tab_col_statistics 
+where  table_name = 'T';
+pause
+
+clear screen
+select max(num),count(*) 
+from t where id > 9000;
+pause
+select *
+from table(dbms_xplan.display_cursor(format=>'typical'));
+
---
+++ office-hours/clean.sql
@@ -0,0 +1,11 @@
+set termout off
+alter session set nls_date_format = 'DD-MON-RR';
+set autotrace off
+set timing off
+set sqlprompt 'SQL> '
+set echo off
+set feedback on
+clear screen
+set termout on
+set define '&'
+set tab off
