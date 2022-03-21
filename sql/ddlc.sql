09632d563f4182eb19eed5f4b879f6449c2f295b
Connor McDonald <connor_mcdonald@yahoo.com>
2022-03-21 11:17:20 +0800

    Add ddlf, ddlc to misc scripts

--- sql/ddlc.sql
+++ sql/ddlc.sql
@@ -1,3 +1,13 @@
+-------------------------------------------------------------------------------
+--
+-- PLEASE NOTE
+-- 
+-- No warranty, no liability, no support.
+--
+-- This script is 100% at your own risk to use.
+--
+-------------------------------------------------------------------------------
+
 undefine object_name
 undefine owner
 
